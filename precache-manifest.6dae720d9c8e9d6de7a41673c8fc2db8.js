0aea65537bb55f60fbb39c5f080f9387c365b924
LAPTOP-IKDU3139\ADMIN <hunglt5@vng.com.vn>
2020-09-24 19:52:29 +0700

    Updates

---
+++ precache-manifest.6dae720d9c8e9d6de7a41673c8fc2db8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1117e82c0e9162fd5a7c8dc4560acc71",
+    "url": "/react_products_management/index.html"
+  },
+  {
+    "revision": "a285eb8f3fd619115da3",
+    "url": "/react_products_management/static/css/main.c5d8f449.chunk.css"
+  },
+  {
+    "revision": "bb249ecf993c47518b92",
+    "url": "/react_products_management/static/js/2.3cbb8490.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/react_products_management/static/js/2.3cbb8490.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a285eb8f3fd619115da3",
+    "url": "/react_products_management/static/js/main.fbaeb584.chunk.js"
+  },
+  {
+    "revision": "e7c41fe49a09107b5166",
+    "url": "/react_products_management/static/js/runtime-main.0ef43213.js"
+  }
+]);
